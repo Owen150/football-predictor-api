7a86aabf9cb87d1cd37e405cea1b868d71a39330
Owen150 <michaeloduor50@gmail.com>
2026-09-15 21:36:08 +0300

    Creaded Fixtures Endpoint

--- src/main/java/com/footballpredictor/api/controller/FixtureController.java
+++ src/main/java/com/footballpredictor/api/controller/FixtureController.java
@@ -1,4 +1,21 @@
 package com.footballpredictor.api.controller;
+import com.footballpredictor.api.client.SportmonksClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/api/v1/fixtures")
+@RequiredArgsConstructor
 public class FixtureController {
+    private final SportmonksClient sportmonksClient;
+
+    @GetMapping("/today")
+    public String getTodayFixtures() {
+
+        return sportmonksClient.getFixturesByDate(
+                LocalDate.now().toString()
+        );
+    }
 }
